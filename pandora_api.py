7d93e63f4290edf5126db31ddabee9d3aa16209b
Adam <adamjsimon1@yahoo.com>
2018-03-05 20:22:27 -0500

    getting an error with httpsconnectionpool

--- pandora_api.py
+++ pandora_api.py
@@ -7,7 +7,8 @@ passwrd = 'gizmos'
 headers = {
     'Content-type': 'application/json;charset-utf-8',
     'X-CsrfToken': '',
-    'X-AuthToken': ''
+    'X-AuthToken': '',
+    'host': 'https://www.pandora.com'
 }
 data = {
     'existingAuthToken': None,
@@ -17,18 +18,19 @@ data = {
 }
 cookies = {}
 
-host='https://www.pandora.com'
-main_api_endpoint = host+'/api'
-auth_endpoint = host+main_api_endpoint+'/v1/auth/login'
+url =  'https://www.pandora.com'
+main_api_endpoint = url + '/api'
+auth_endpoint = url + main_api_endpoint + '/v1/auth/login'
 
 # get csrf token
-resp_csrf = requests.head(host)
+resp_csrf = requests.head(url)
 cookies['csrftoken'] = resp_csrf.cookies['csrftoken']
 headers['X-CsrfToken'] = resp_csrf.cookies['csrftoken']
 
 # get auth token
-resp_auth = requests.post(auth_endpoint,
-                         data=data,
-                         headers=headers,
-                         cookies=cookies)
+try:
+    resp_auth = requests.post(auth_endpoint,data=data,headers=headers,cookies=cookies)
+except Exception as e:
+    print(type(e))
+    print(e)
 headers['X-AuthToken'] = resp_auth.text['']
